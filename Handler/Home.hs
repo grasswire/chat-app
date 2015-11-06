6b0591848b8575ecc01b15e139f77104c7f103e5
Levi Notik <levinotik@gmail.com>
2015-11-05 23:59:13 -0500

    remove unused imports and functions

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -5,38 +5,8 @@ module Handler.Home where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,
                               withSmallInput)
--- import Yesod.Core
 import Yesod.WebSockets
-import qualified Data.Text.Lazy as TL
-import Control.Monad (forever)
-import Control.Monad.Trans.Reader
-import Control.Concurrent (threadDelay)
-import Data.Time
-import Conduit
--- import Data.Monoid ((<>))
-import Control.Concurrent.STM.Lifted hiding (atomically)
-import Data.Text (Text)
 
--- data MyApp = MyApp (TChan Text)
---
--- instance Yesod MyApp
-
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
--- getHomeR :: Handler Html
--- getHomeR = do
---     (formWidget, formEnctype) <- generateFormPost sampleForm
---     let submission = Nothing :: Maybe (FileInfo, Text)
---         handlerName = "getHomeR" :: Text
---     defaultLayout $ do
---         aDomId <- newIdent
---         setTitle "Welcome To Yesod!"
---         $(widgetFile "homepage")
 
 chatApp :: WebSocketsT Handler ()
 chatApp = do
@@ -44,11 +14,11 @@ chatApp = do
     name <- receiveData
     sendTextData $ "Welcome, " <> name
     app <- getYesod
-    readChan <- atomically $ do
+    inChan <- atomically $ do
         writeTChan (appWriteChan $ app) $ name <> " has joined the chat"
         dupTChan (appWriteChan $ app)
     race_
-        (forever $ atomically (readTChan readChan) >>= sendTextData)
+        (forever $ atomically (readTChan inChan) >>= sendTextData)
         (sourceWS $$ mapM_C (\msg ->
             atomically $ writeTChan (appWriteChan $ app) $ name <> ": " <> msg))
 
@@ -103,69 +73,6 @@ getChatR = do
                 e.preventDefault();
             });
         |]
-
-getHomeR :: Handler Html
-getHomeR = do
-    webSockets chatApp
-    defaultLayout $ do
-        [whamlet|
-            <div #output>
-            <form #form>
-                <input #input autofocus>
-        |]
-        toWidget [lucius|
-            \#output {
-                width: 600px;
-                height: 400px;
-                border: 1px solid black;
-                margin-bottom: 1em;
-                p {
-                    margin: 0 0 0.5em 0;
-                    padding: 0 0 0.5em 0;
-                    border-bottom: 1px dashed #99aa99;
-                }
-            }
-            \#input {
-                width: 600px;
-                display: block;
-            }
-        |]
-        toWidget [julius|
-            var url = document.URL,
-                output = document.getElementById("output"),
-                form = document.getElementById("form"),
-                input = document.getElementById("input"),
-                conn;
-
-            url = url.replace("http:", "ws:").replace("https:", "wss:");
-            conn = new WebSocket(url);
-
-            conn.onmessage = function(e) {
-                var p = document.createElement("p");
-                p.appendChild(document.createTextNode(e.data));
-                output.appendChild(p);
-            };
-
-            form.addEventListener("submit", function(e){
-                conn.send(input.value);
-                input.value = "";
-                e.preventDefault();
-            });
-        |]
-
-
--- postHomeR :: Handler Html
--- postHomeR = do
---     ((result, formWidget), formEnctype) <- runFormPost sampleForm
---     let handlerName = "postHomeR" :: Text
---         submission = case result of
---             FormSuccess res -> Just res
---             _ -> Nothing
---
---     defaultLayout $ do
---         aDomId <- newIdent
---         setTitle "Welcome To Yesod!"
---         $(widgetFile "homepage")
 
 sampleForm :: Form (FileInfo, Text)
 sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
