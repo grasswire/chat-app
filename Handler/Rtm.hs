525ecab4501e37e69ac7b374286933f8ff9983b5
Levi Notik <levinotik@gmail.com>
2015-11-22 21:33:50 -0500

    remove unused imports. clean up text read

--- Handler/Rtm.hs
+++ Handler/Rtm.hs
@@ -3,14 +3,12 @@
 module Handler.Rtm where
 
 import Import
-import Yesod.WebSockets
 import Server
 
-import Taplike.Shared (RtmStartRp, RtmStartRp(..), Self(..))
-import Database.Persist.Sql (fromSqlKey, toSqlKey)
-import Data.Aeson (Value)
-import qualified Data.Text.Read as TR
-import Text.Julius (rawJS)
+import Database.Persist.Sql (toSqlKey)
+import Taplike.Shared (RtmStartRp(..), Self(..))
+import Text.Read (readEither)
+
 
 getRtmStartR :: Handler Value
 getRtmStartR = do
@@ -22,25 +20,25 @@ getRtmStartR = do
       mparam <- lookupGetParam "room_id"
       case mparam of
         Just roomId -> do
-          let maybeInt = TR.decimal roomId
+          let maybeInt = readEither (unpack roomId) :: Either String Int64
           case maybeInt of
             Right rId -> do
               renderer <- getUrlRender
-              let sqlKey = toSqlKey ((fromIntegral . fst $ rId ) :: Int64)
+              let sqlKey = toSqlKey rId -- ((fromIntegral . fst $ rId ) :: Int64)
               let url = renderer $ ChatR sqlKey
               chatRoom <- runDB (get sqlKey)
               users <- case chatRoom of
                         Just room -> do
                           userIds <- do
-                                      channel <- liftIO $ atomically $ lookupChannel (chatServer app) (fromStrict $ chatRoomTitle room)
-                                      case channel of
-                                        Just ch -> liftIO $ atomically (listUsers ch)
-                                        _ -> return []
+                            channel <- liftIO $ atomically $ lookupChannel (chatServer app) (fromStrict $ chatRoomTitle room)
+                            case channel of
+                              Just ch -> liftIO $ atomically (listUsers ch)
+                              _ -> return []
                           runDB $ selectList [UserTwitterUserId <-. userIds] []
                         _ -> return []
               let jsonResp = case user of
                               Just u -> RtmStartRp url (Just $ Self (unUserKey i) (userTwitterScreenName $ entityVal u) (userProfileImageUrl $ entityVal u)) (fmap entityVal users)
-                              _ -> RtmStartRp url Nothing (fmap entityVal users)
-              returnJson $ jsonResp
+                              _      -> RtmStartRp url Nothing (fmap entityVal users)
+              returnJson jsonResp
             Left err -> sendResponseStatus badRequest400 (pack err :: Text)
         Nothing -> sendResponseStatus badRequest400 ("BADREQUEST: MISSING room_id param" :: Text)
