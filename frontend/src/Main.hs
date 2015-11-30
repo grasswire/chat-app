faaf8b33e0f9b99e2ffd2180cf72dc68734bd7f1
Chris Allen <cma@bitemyapp.com>
2015-11-30 12:02:40 -0600

    blah

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -1,25 +1,88 @@
+{-# LANGUAGE RecursiveDo #-}
+
 module Main (
     main
 ) where
 
-import Control.Applicative ((<$>))
-import GHCJS.DOM
-       (enableInspector, webViewGetDomDocument, runWebGUI)
-import GHCJS.DOM.Document (getBody, createElement, createTextNode, click)
-import GHCJS.DOM.Element (setInnerHTML)
-import GHCJS.DOM.Node (appendChild)
-import GHCJS.DOM.EventM (on, mouseClientXY)
+-- import Control.Applicative ((<$>))
+-- import GHCJS.DOM
+--        (enableInspector, webViewGetDomDocument, runWebGUI)
+-- import GHCJS.DOM.Document (getBody, createElement, createTextNode, click)
+-- import GHCJS.DOM.Element (setInnerHTML)
+-- import GHCJS.DOM.Node (appendChild)
+-- import GHCJS.DOM.EventM (on, mouseClientXY)
+import Reflex
+import Reflex.Dom
+import qualified Data.Map as Map
+import Safe (readMay)
 
-main = runWebGUI $ \ webView -> do
-    enableInspector webView
-    Just doc <- webViewGetDomDocument webView
-    Just body <- getBody doc
-    setInnerHTML body (Just "<h1>Hello World</h1>")
-    on doc click $ do
-        (x, y) <- mouseClientXY
-        Just newParagraph <- createElement doc (Just "p")
-        text <- createTextNode doc $ "Click " ++ show (x, y)
-        appendChild newParagraph text
-        appendChild body (Just newParagraph)
-        return ()
-    return ()
+
+-- main :: IO ()
+-- main = mainWidget $ el "div" $ do
+--   rec _ <- dynText $ _textInput_value t
+--       t <- textInput def
+--   return ()
+
+-- main :: IO ()
+-- main = mainWidget $ el "div" $ do
+--   rec _ <- dynText $ _textInput_value t
+--       t <- el "div" $ do
+--         textInput def
+--   return ()
+
+main :: IO ()
+main = mainWidget $ el "div" $ do
+  rec _ <- dynText $ _textInput_value t
+      t <- el "div" $ do
+        textInput def
+  return ()
+
+-- main :: IO ()
+-- main = mainWidget $ el "div" $ do
+--   nx <- numberInput
+--   d <- dropdown "*" (constDyn ops) def
+--   ny <- numberInput
+--   values <- combineDyn (,) nx ny
+--   result <- combineDyn (\o (x, y) -> stringToOp o <$> x <*> y) (_dropdown_value d) values
+--   resultString <- mapDyn show result
+--   text " = "
+--   dynText resultString
+
+-- numberInput :: MonadWidget t m => m (Dynamic t (Maybe Double))
+-- numberInput = do
+--   n <- textInput $ def & textInputConfig_inputType .~ "number"
+--                        & textInputConfig_initialValue .~ "0"
+--   mapDyn readMay $ _textInput_value n
+
+-- numberInput :: MonadWidget t m => m (Dynamic t (Maybe Double))
+-- numberInput = do
+--   let attrs = constDyn $ Map.fromList [("style", "border-color: blue")]
+--   n <- textInput $ def & textInputConfig_inputType .~ "number"
+--                        & textInputConfig_initialValue .~ "0"
+--                        & textInputConfig_attributes .~ attrs
+--   mapDyn readMay $ _textInput_value n
+
+-- numberInput :: (MonadWidget t m) => m (Dynamic t (Maybe Double))
+-- numberInput = do
+--   let errorState = Map.singleton "style" "border-color: red"
+--       validState = Map.singleton "style" "border-color: blue"
+--   rec n <- textInput $ def & textInputConfig_inputType .~ "number"
+--                            & textInputConfig_initialValue .~ "0"
+--                            & textInputConfig_attributes .~ attrs
+--       result <- mapDyn readMay $ _textInput_value n
+--       attrs <- mapDyn (\r -> case r of
+--                                   Just _ -> validState
+--                                   Nothing -> errorState) result
+--   return result
+
+-- ops = Map.fromList [ ("+", "+")
+--                    , ("-", "-")
+--                    , ("*", "*")
+--                    , ("/", "/") ]
+
+-- stringToOp s =
+--   case s of
+--     "-" -> (-)
+--     "*" -> (*)
+--     "/" -> (/)
+--     _   -> (+)
