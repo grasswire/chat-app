3644f1c9272f296f61a514bde894daf952751fa7
Joe Cianflone <joe@cianflone.co>
2015-12-01 12:51:35 -0500

    sidebar toggle close on small screens

---
+++ src/js/modules/sidebar.js
@@ -0,0 +1,53 @@
+App.Modules = App.Modules || {};
+App.Modules.Sidebar = function () {
+
+   var o = {
+      threshold: 768
+   };
+
+   var toggleSidebar = function(data) {
+      var container = $(".js-sidebar");
+
+      if (container.attr('data-container-closed') == "true") {
+         container.attr('data-container-closed', "false")
+                  .css({
+                     minWidth: "240px",
+                     overflowX: "hidden",
+                     overflowY: "scroll",
+                     width: "30%",
+                  })
+                  .find(".js-sidebar-contents").show();
+         return false;
+      }
+
+      container.attr('data-container-closed', "true")
+               .css({
+                  minWidth: "auto",
+                  overflow: "hidden",
+                  width: "50px"
+               })
+               .find(".js-sidebar-contents").hide();
+      return false;
+   };
+
+   var checkPageWidth = function() {
+      var container = $(".js-sidebar"),
+          pageWidth = parseInt($("body").outerWidth(true));
+
+      if (container.attr('data-container-closed') != "true" && pageWidth <= o.threshold) {
+         $(".js-sidebar-toggler").trigger("click");
+      }
+   };
+
+
+   return {
+      init: function() { return this; },
+      events: function() {
+         Events.bind("load").where("body[class]", "chatroom").to(checkPageWidth, this);
+         Events.bind("resize").where("body[class]", "chatroom").to(checkPageWidth, this);
+         Events.bind("click", ".js-sidebar-toggler").to(toggleSidebar, this);
+         return this;
+      }
+   };
+
+}();
