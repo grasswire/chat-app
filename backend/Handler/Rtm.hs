bdddbfacdd0b769a8da8e9f3f68e1bb955298dec
Levi Notik <levinotik@gmail.com>
2015-12-04 10:35:07 -0500

    add esqueleto query for users in channel in last five mins. Use query in rtm.start api

--- backend/Handler/Rtm.hs
+++ backend/Handler/Rtm.hs
@@ -3,40 +3,41 @@
 module Handler.Rtm where
 
 import Import
-import qualified Server as S
 
 import Taplike.Shared (RtmStartRp(..), Self(..))
 import Taplike.ChannelSlug
-import Database.Persist.Sql (toSqlKey, fromSqlKey)
-
+import qualified Database.Esqueleto as E
+import Data.Time.Clock
 
 getRtmStartR :: Handler Value
 getRtmStartR = do
   authId <- maybeAuthId
-  app <- getYesod
   user <- case authId of
             Just i -> fmap (Entity i) <$> runDB (get i)
             _      -> return Nothing
-  let key = entityKey <$> user
-  mparam <- lookupGetParam "room_id"
+  mparam <- lookupGetParam "channel_slug"
   case mparam of
-    Just roomId -> do
-      let chatSlug = ChannelSlug roomId
+    Just slug -> do
       renderer <- getUrlRender
-      let url = renderer $ ChatR chatSlug
-      channel <- runDB (getBy $ UniqueChannelSlug chatSlug)
-      users <- case channel of
-                Just room -> do
-                  userIds <- do
-                    serverChannel <- liftIO $ atomically $ S.lookupChannel (chatServer app) (entityKey room)
-                    case serverChannel of
-                      Just ch -> liftIO $ atomically (S.listUsers ch)
-                      _ -> return []
-                  -- runDB $ selectList [UserTwitterUserId <-. userIds] []
-                  return []
-                _ -> return []
+      let channelSlug = ChannelSlug slug
+          url = renderer $ ChatR channelSlug
+      maybeChan <- runDB (getBy $ UniqueChannelSlug channelSlug)
+      users <- case maybeChan of
+                Just channel -> do
+                  timeNow <- liftIO $ getCurrentTime
+                  let fiveMinsAgo = addUTCTime ((negate 5) :: NominalDiffTime) timeNow
+                  runDB (usersPresentQuery (entityKey channel) fiveMinsAgo)
+                _ ->  return []
       let jsonResp = case user of
                       Just u -> RtmStartRp url (Just $ Self (entityKey u) (userTwitterScreenName $ entityVal u) (userProfileImageUrl $ entityVal u)) (fmap entityVal users)
                       _      -> RtmStartRp url Nothing (fmap entityVal users)
       returnJson jsonResp
-    Nothing -> sendResponseStatus badRequest400 ("BADREQUEST: MISSING room_id param" :: Text)
+    Nothing -> sendResponseStatus badRequest400 ("BADREQUEST: MISSING channel_slug param" :: Text)
+
+usersPresentQuery ::  MonadIO m => Key Channel -> UTCTime -> SqlPersistT m [Entity User]
+usersPresentQuery chanKey lastseen = E.select $
+                                     E.from $ \user -> do
+                                     E.where_ $ E.exists $
+                                                E.from $ \heartbeat -> do
+                                                E.where_ (heartbeat E.^. HeartbeatChannel E.==. (E.val chanKey) E.&&. heartbeat E.^. HeartbeatUser E.==. user E.^. UserId E.&&. heartbeat E.^. HeartbeatLastSeen E.>=. (E.val lastseen))
+                                     return user
