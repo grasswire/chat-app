15a54589ea75763886bcb60ac16a4c8caaed4937
Joe Cianflone <joe@cianflone.co>
2015-12-07 12:51:30 -0500

    move text blurbs to handlebars

--- backend/src/js/modules/chat-messages.js
+++ backend/src/js/modules/chat-messages.js
@@ -25,25 +25,40 @@ App.Modules.ChatMessages = function () {
 
       o.connection.onmessage = function(e) {
          Events.publish("tl/chat/message/sent", {
-            e: e
+            message: e
          });
       };
    };
 
    var displayMessage = function(response) {
-     var p = document.createElement("h2");
-     p.appendChild(document.createTextNode(response.e.data));
+      $('.js-chat-output').append(Handlebars.templates.blurb(response.message));
+      return false;
+   };
 
-     $(".js-chat-output").append(p);
+   var messageReceived = function(response) {
+      try {
+         var msg = JSON.parse(response.message.data);
+         var message = {
+            text: msg.text
+         };
 
-     return false;
+      } catch (e) {
+         var message = {
+            text: response.message.data
+         };
+      };
+
+      Events.publish("tl/chat/message/received", {
+         message: message
+      });
    };
 
    return {
       init: function() { return this; },
       events: function() {
          Events.subscribe("tl/chat/setup", chatReady);
-         Events.subscribe("tl/chat/message/sent", displayMessage);
+         Events.subscribe("tl/chat/message/sent", messageReceived);
+         Events.subscribe("tl/chat/message/received", displayMessage);
 
          Events.bind("submit", ".js-chat-form").to(sendMessage, this);
          return this;
