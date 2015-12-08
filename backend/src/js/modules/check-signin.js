03103c7981c7a4401bc3234b1b94481f3676a41a
Joe Cianflone <joe@cianflone.co>
2015-12-08 12:18:50 -0500

    cleanup signin modal a bit more

--- backend/src/js/modules/check-signin.js
+++ backend/src/js/modules/check-signin.js
@@ -3,26 +3,25 @@ App.Modules.CheckSignin = function () {
 
    var o = {};
 
-   var checkLogin = function() {
-      var isLoggedIn = ($('body').data('is-logged-in').toLowerCase() === "true") ? true : false;
+   var alertUserToLogin = function(data) {
+      if ($('body').data('is-logged-in').toLowerCase() !== "true") {
 
-      if (! isLoggedIn) {
+         if (! _.isUndefined(data.eventElement)) {
+            data.eventElement.blur();
+         }
+
          Events.publish("tl/modal/open", {
             modal: 'signin'
          });
       }
    };
 
-   var noWriting = function(data) {
-      checkLogin();
-      data.eventElement.blur();
-   };
-
    return {
       init: function() { return this; },
       events: function() {
-         Events.bind("load").where('body[class]', 'chatroom').to(checkLogin, this);
-         Events.bind("focusin", ".js-chat-input").to(noWriting, this);
+         Events.bind("load").where('body[class]', 'chatroom').to(alertUserToLogin, this);
+         Events.bind("focusin", ".js-chat-input").where('body[class]', 'chatroom').to(alertUserToLogin, this);
+
          return this;
       }
    };
