3c25177d47098dbfbc6f4d9a62baaa463e0eb71d
Joe Cianflone <joe@cianflone.co>
2015-12-11 18:37:55 -0500

    WIP: chat started

--- backend/Handler/Chat.hs
+++ backend/Handler/Chat.hs
@@ -66,12 +66,12 @@ chatApp exceptionHandler channelId channelSlug userEntity =
             let processedMsg = processMessage (entityKey user) inEvent ts
             maybe (return ()) (\e -> void (liftIO $ runRedisAction redisConn $ S.broadcastEvent channelSlug e) >> persistEvent (entityKey user) e) processedMsg
           pure ()
-    
+
     persistEvent :: UserId -> RtmEvent -> Handler ()
-    persistEvent userId event = case event of 
+    persistEvent userId event = case event of
                                   RtmMessage msg -> void $ runDB $ insert (Message userId (SH.unMessageText $ SH.messageText msg) (SH.messageTS msg) channelId (MessageUUID $ SH.messageUUID msg))
-                                  _              -> return ()  
-    
+                                  _              -> return ()
+
     ackMessage (RtmSendMessage incoming) = do
       now <- liftIO getCurrentTime
       sendTextData (RtmReplyOk (ReplyOk (SH.incomingMessageUUID incoming) (Just now) (Just $ SH.unMessageText $ incomingMessageMessageText incoming)))
@@ -104,8 +104,6 @@ getChatR slug = do
     case channel of
       Just c -> do
         let room = entityVal c
-            masthead = $(widgetFile "partials/chat/masthead")
-            sidebar  = $(widgetFile "partials/chat/sidebar")
             isLoggedIn = isJust authId
             chanSlug = channelCrSlug $ room
         webSockets $ chatApp (wsExceptionHandler (redisConn app) chanSlug) (entityKey c) chanSlug chatUser
@@ -140,22 +138,22 @@ getHomeR = do
     let modalCreate = $(widgetFile "partials/modals/create")
     timeNow <- liftIO getCurrentTime
     let minActiveAgo = addUTCTime (negate 3600 :: NominalDiffTime) timeNow
-    (topChannels, allChannels) <- do 
+    (topChannels, allChannels) <- do
         chanEntities <- runDB (popularChannels minActiveAgo)
         presences <- liftIO $ runRedisAction (redisConn app) $ getPresenceForChannels (channelCrSlug <$> entityVal <$> chanEntities)
-        let zipped = case presences of 
+        let zipped = case presences of
                       Right ps -> chanEntities `zip` ps
-                      Left _   -> chanEntities `zip` (replicate (length chanEntities) (TP.NumberUsersPresent 0))      
+                      Left _   -> chanEntities `zip` (replicate (length chanEntities) (TP.NumberUsersPresent 0))
         return $ splitAt 9 $ sortBy (flip compare `on` TP.channelNumUsersPresent ) $ (\(c, numPresent) -> chanFromEntity c numPresent) <$> zipped
     defaultLayout $ do
       setTitle "Taplike / Home"
       $(widgetFile "homepage")
-      
+
 popularChannelsStatement :: Text
 popularChannelsStatement = "select ?? from channel where id in (select channel from message where timestamp >= ? group by channel order by count(*) desc limit 27);"
 
 popularChannels :: MonadIO m => UTCTime -> ReaderT SqlBackend m [Entity Channel]
-popularChannels since = rawSql popularChannelsStatement [PersistUTCTime since]      
+popularChannels since = rawSql popularChannelsStatement [PersistUTCTime since]
 
 getLogOutR :: Handler Html
 getLogOutR = clearSession >> getHomeR
