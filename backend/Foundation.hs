09d02306253ae7fe09a70677afcaf849b95734e5
Levi Notik <levinotik@gmail.com>
2015-12-17 15:35:34 -0500

    set session timeout to 30 days

--- backend/Foundation.hs
+++ backend/Foundation.hs
@@ -70,7 +70,7 @@ instance Yesod App where
     -- Store session data on the client in encrypted cookies,
     -- default session idle timeout is 120 minutes
     makeSessionBackend _ = Just <$> defaultClientSessionBackend
-        120    -- timeout in minutes
+        43200    -- timeout in minutes
         "config/client_session_key.aes"
 
     defaultLayout widget = do
